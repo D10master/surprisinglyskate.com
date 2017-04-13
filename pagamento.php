129f4c97ae476513a0bfce61320600d1398a6658
DANI-PC\Dani <pinocorri@gmail.com>
2017-04-13 21:15:54 +0200

    aggiunta query inserimento ordine e detteglio ordine(non finita)

--- pagamento.php
+++ pagamento.php
@@ -1,10 +1,13 @@
 <?php
-	//chiamata al DB
   session_start();
+
+  //chiamata al DB
 	include('connect.php');
+
   $titolare="";
   $id_utente = $_SESSION['id'];
 
+  // QUERY PER RICAVARE L'ID DEL CLIENTE DAL ID DEL UTENTE
 	$query="";
 	$query=$query."SELECT id_cliente  FROM dati_accesso WHERE id_cliente = ".$_SESSION['id'];
 	$rid_cliente=mysql_query($query) or die($query);
@@ -16,12 +19,27 @@
     }
   }
 
+  //CARICAMENTO CARTE
   $query="";
   $query=$query . "SELECT pagamento.titolare, pagamento.id, pagamento.codice, pagamento.scadenza, carte.nome FROM pagamento " ;
   $query=$query . "INNER JOIN carte ON pagamento.id_carta = carte.id ";
   $query=$query . "WHERE id_cliente = " . $id_cliente." ";
   $rcarte=mysql_query($query) or die($query);
 
+ //INSERIMENTO ORDINE
+  if(isset($_POST['paga'])){
+    $query="";
+    $query=$query . "INSERT INTO ordini (data,stato,id_cliente) " ;
+    $query=$query . "VALUES (now(),1,'".$id_cliente."')";
+    $rordine=mysql_query($query) or die($query);
+
+    /*
+    $query="";
+    $query=$query . "INSERT INTO dettaglio_ordine (id_ordine,i_prodotto,quantita) ";
+    $query=$query . "VALUES ()";
+    $rordine=mysql_query($query) or die($query);
+    */
+  }
 
 
 ?>
@@ -38,6 +56,7 @@
     <DIV id='content'>
 
 			<?php
+      // CARICO LE CARTE IN UNA SELECT
 			echo "<FORM method='post' action=''>";
 			echo "<SELECT name='carta'>";
 			echo "<OPTION value=''>Seleziona</OPTION>";
@@ -49,15 +68,11 @@
 			$codice = $array['codice'];
       $scadenza = date($array['scandenza']);
       $tipo_carta = $array['nome'];
-		     echo "<OPTION value='".$id."'> carta:".$tipo_carta." " . $codice ." ". $scadenza . "</OPTION>";
+		     echo "<OPTION value='".$id."'> carta:".$tipo_carta." " . $codice .", ". $scadenza .", Intestatario carta:".$titolare."<br/>". "</OPTION>";
 	   }
 	   echo "</SELECT> <br>";
-     echo "Intestatario carta:".$titolare."<br/>";
      echo "<INPUT id='' name='paga'  type='submit' value='Paga'></INPUT>";
      echo "</FORM>";
-     echo "scadenza = ".$scadenza;
-     ?>
-
      </DIV>
 		<?php include("footer.php"); ?>
 	</BODY>
