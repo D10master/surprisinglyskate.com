a749c2a2b4ceafbda0673f8144186acde02ce405
DANI-PC\Dani <pinocorri@gmail.com>
2017-04-14 15:07:58 +0200

    aggiunta ricerca per nome

--- prodotti.php
+++ prodotti.php
@@ -2,19 +2,31 @@
 	//chiamata al DB
 	include('connect.php');
 	include ('ProductCard.php');
-	if(!isset($_POST['cerca_categoria'])){
-		$query="";
-		$query=$query."SELECT id,nome,prezzo,produttore FROM prodotti ";
-	}else {
+
+	$query="";
+	$query=$query."SELECT id,nome,prezzo,produttore FROM prodotti ";
+
+
+	if(isset($_POST['cerca_categoria'])){
 		$query="";
 		$query=$query."SELECT id,nome,prezzo,produttore FROM prodotti ";
 		$query=$query."WHERE id_categoria='".$_POST['categoria']."'";
 	}
+
+	if(isset($_POST['search_button'])){
+		$query="";
+		$query=$query."SELECT id,nome,prezzo,produttore FROM prodotti ";
+		$query=$query."WHERE nome LIKE '%".$_POST['search_field']."%'";
+		echo $query;
+	}
+
+
 	$risultato=mysql_query($query) or die($query);
 
 	$query="";
 	$query=$query."SELECT id,nome FROM categorie ";
 	$rcategorie=mysql_query($query) or die($query);
+
 
 ?>
 
@@ -29,7 +41,7 @@
 		?>
 
 		<DIV id='content'>
-			<FORM method='post' action=''>
+			<FORM method='post' action='prodotti.php'>
 				<INPUT id='search_field' name='search_field' placeholder='Cerca...' type='text'></INPUT>
 				<INPUT id='search_button' name='search_button'  type='submit' value=''></INPUT>
 			</FORM>
