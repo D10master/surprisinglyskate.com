f2efd55b3c0aa98c75e59a8bbaf9a89d26461ee9
D10master <chicco.romeo@alice.it>
2017-04-14 22:50:42 +0200

    Aggiunto file "script.js"
    in oltre ho raggruppato tutti gli elementi del login in un unico form

--- login.php
+++ login.php
@@ -7,27 +7,30 @@
 
 ?>
 
+
+
 <HTML>
 
 <HEAD>
 <LINK rel='Stylesheet' href='stile.css' type='TEXT/CSS'>
 </HEAD>
 
+
 <BODY>
 <H2>Login</H2>
 
+
 <?php
 
-echo "<FORM name='F1' method='post' action='index.php'>";
-echo "username:<INPUT type='text' name='USER' value='' size='4'><BR>";
-echo "password:<INPUT type='text' name='PASS' value='' size='4'><BR><BR>";
-echo "<INPUT class='bottone' type='submit' name='ENTRA' value='Login'>";
-echo "</FORM>";
-
-echo "<FORM name='F2' method='post' action='registra.php'>";
-echo "<INPUT class='bottone' type='submit' name='REGISTRA' value='Registrati'><BR>";
+echo "<FORM name='F1' method='post' action='index.php' id='login'>";
+	echo "username:<INPUT type='text' name='USER' value='' size='4'><BR>";
+	echo "password:<INPUT type='text' name='PASS' value='' size='4'><BR><BR>";
+	echo "<INPUT class='bottone' type='submit' name='ENTRA' value='Login'>";
+	echo "<INPUT class='bottone' type='submit' name='REGISTRA' value='Registrati'><BR>";
 echo "</FORM>";
 ?>
 
+
 </BODY>
 </HTML>
+
