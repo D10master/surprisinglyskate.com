c3234e2bd11b127f542b7bf1675f8f345e92f2d9
D10master <chicco.romeo@alice.it>
2017-04-17 17:24:25 +0200

    Corretto bug bottone chiusura login
    Ho corretto il problema del bottone di chiusura del login per cui mi
    mandava sempre sulla pagina iniziale senza eseguire l'animazione

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function openLogin()
 {
 	document.getElementById("login").style.transform = "scale(1.0,1.0)";
 	document.getElementById("login").style.animationName = "open_login";
+	document.getElementById("login").style.animationDuration= "0.7s";
 	document.getElementById("login").style.animationPlayState = "running";
 }
 
@@ -9,5 +10,6 @@ function closeLogin()
 {
 	document.getElementById("login").style.transform = "scale(0.0,0.0)";
 	document.getElementById("login").style.animationName = "close_login";
+	document.getElementById("login").style.animationDuration= "0.5s";
 	document.getElementById("login").style.animationPlayState = "running";
 }
